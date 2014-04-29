3050910d9d656e50191e5a536cea88814d713409
John He <hejiangx@gmail.com>
2014-04-29 08:33:10 -0700

    Adjust Makefile, make echo work.

--- lib/error.c
+++ lib/error.c
@@ -35,7 +35,7 @@ ssize_t writen(int fd, const void *vptr, size_t n)
 
     ptr = vptr;
     nleft = n;
-    while (nleft < 0)
+    while (nleft > 0)
     {
         if( (nwritten = write(fd, ptr, nleft)) <= 0)
         {
